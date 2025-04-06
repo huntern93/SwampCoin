828580ef22230f766f8fc26eca4a8b1a57797e18
huntern93 <nathan.james.hunter@gmail.com>
2025-04-06 14:41:22 +1000

    Update index.jsx

--- src/Components/JoinCommunity/index.jsx
+++ src/Components/JoinCommunity/index.jsx
@@ -33,7 +33,7 @@ export const JoinCommunity = ({ size, className }) => {
           </clipPath>
         </defs>
       </svg>,
-      link: "https://discord.gg/CbW5GbwS"
+      link: "https://discord.gg/eQ3GFhVe9M"
     },
     {
       icon: <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
